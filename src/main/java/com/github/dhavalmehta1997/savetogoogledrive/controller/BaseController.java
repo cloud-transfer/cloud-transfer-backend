583ab4ffb46ff5a214ed69e5c9f1720732f2b8e6
Dhaval Mehta <dhaval.mehta197@gmail.com>
2017-09-27 20:23:23 +0530

    Create base controller

---
+++ src/main/java/com/github/dhavalmehta1997/savetogoogledrive/controller/BaseController.java
@@ -0,0 +1,7 @@
+package com.github.dhavalmehta1997.savetogoogledrive.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/api")
+public abstract class BaseController {
+}
