f13d111dc1e9e386a19f98ffa679269f8f8f2f8f
Dhaval Mehta <dhaval.mehta197@gmail.com>
2017-09-27 20:31:10 +0530

    add spring annotation configuration

---
+++ src/main/java/com/github/dhavalmehta1997/savetogoogledrive/config/WebApplicationConfiguration.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.dhavalmehta1997.savetogoogledrive.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+/**
+ * @author Dhaval
+ */
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.github.dhavalmehta1997.savetogoogledrive.controller")
+public class WebApplicationConfiguration extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver
+                = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/views/jsp/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+}
