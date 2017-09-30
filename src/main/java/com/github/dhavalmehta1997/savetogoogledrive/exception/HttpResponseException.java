4b9ad9cfea00b1fab08f2e171e2252f9c0290294
Dhaval Mehta <dhaval.mehta197@gmail.com>
2017-10-01 00:36:10 +0530

    optimize logic

--- src/main/java/com/github/dhavalmehta1997/savetogoogledrive/exception/HttpResponseException.java
+++ src/main/java/com/github/dhavalmehta1997/savetogoogledrive/exception/HttpResponseException.java
@@ -3,30 +3,28 @@ package com.github.dhavalmehta1997.savetogoogledrive.exception;
 import org.springframework.http.HttpStatus;
 
 public class HttpResponseException extends RuntimeException {
-    HttpStatus status;
+    private HttpStatus status;
 
-    public HttpResponseException(int statusCode) {
-        this.status = HttpStatus.valueOf(statusCode);
+    public HttpResponseException(HttpStatus status) {
+        this(status, null, null);
     }
 
-    public HttpResponseException(int statusCode, String message) {
-        super(message);
-        this.status = HttpStatus.valueOf(statusCode);
+    public HttpResponseException(HttpStatus status, String message) {
+        this(status, message, null);
     }
 
-    public HttpResponseException(int statusCode, String message, Throwable cause) {
+    public HttpResponseException(HttpStatus status, String message, Throwable cause) {
         super(message, cause);
-        this.status = HttpStatus.valueOf(statusCode);
+        this.status = status;
     }
 
-    public HttpResponseException(int statusCode, Throwable cause) {
-        super(cause);
-        this.status = HttpStatus.valueOf(statusCode);
+    public HttpResponseException(HttpStatus status, Throwable cause) {
+        this(status, null, cause);
     }
 
-    public HttpResponseException(int statusCode, String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+    public HttpResponseException(HttpStatus status, String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
         super(message, cause, enableSuppression, writableStackTrace);
-        this.status = HttpStatus.valueOf(statusCode);
+        this.status = status;
     }
 
     @Override
