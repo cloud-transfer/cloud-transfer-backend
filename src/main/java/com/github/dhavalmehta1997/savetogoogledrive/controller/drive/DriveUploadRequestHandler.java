8cb64b6f36adc7fb1490d08273bad1d0a062caf7
Dhaval Mehta <dhaval.mehta197@gmail.com>
2017-10-02 11:19:00 +0530

    bug fix

--- src/main/java/com/github/dhavalmehta1997/savetogoogledrive/controller/drive/DriveUploadRequestHandler.java
+++ src/main/java/com/github/dhavalmehta1997/savetogoogledrive/controller/drive/DriveUploadRequestHandler.java
@@ -1,7 +1,7 @@
 package com.github.dhavalmehta1997.savetogoogledrive.controller.drive;
 
 import com.github.dhavalmehta1997.savetogoogledrive.controller.BaseController;
-import com.github.dhavalmehta1997.savetogoogledrive.exception.HttpResponseException;
+import com.github.dhavalmehta1997.savetogoogledrive.exception.ApiException;
 import com.github.dhavalmehta1997.savetogoogledrive.model.User;
 import com.github.dhavalmehta1997.savetogoogledrive.uploader.UploadManager;
 import com.github.dhavalmehta1997.savetogoogledrive.uploader.UploadTask;
@@ -10,14 +10,22 @@ import com.github.dhavalmehta1997.savetogoogledrive.uploader.drive.DriveUploader
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URL;
+import java.net.UnknownHostException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @RestController
 public class DriveUploadRequestHandler extends BaseController {
@@ -29,9 +37,17 @@ public class DriveUploadRequestHandler extends BaseController {
         this.session = session;
     }
 
-    @RequestMapping("/drive/upload")
-    public ResponseEntity<String> handleUploadRequest(@RequestParam("url") URL url, @RequestParam(value = "filename", required = false) String filename) {
+    @PostMapping("/drive/upload")
+    public ResponseEntity<String> handleUploadRequest(@RequestParam("url") String urlString, @RequestParam(value = "filename", required = false) String filename, HttpServletResponse response) {
         User user = (User) session.getAttribute("user");
+        URL url;
+
+        try {
+            url = new URL(urlString);
+        } catch (MalformedURLException ex) {
+            throw new ApiException(HttpStatus.UNPROCESSABLE_ENTITY, "URL is Malformed", ex);
+        }
+
         try {
             Uploader uploader = new DriveUploaderBuilder()
                     .setUploadUrl(url)
@@ -43,12 +59,24 @@ public class DriveUploadRequestHandler extends BaseController {
             UploadManager.getUploadManager().add(uploadTask);
             List<String> uploads = (List<String>) session.getAttribute("uploads");
             uploads.add(uploadTask.getId());
-            return new ResponseEntity<>(uploadTask.getId(), HttpStatus.OK);
 
-        } catch (java.net.UnknownHostException ex) {
-            throw new HttpResponseException(HttpStatus.BAD_REQUEST, url.getHost() + ": website not found. ", ex);
+            URI location = ServletUriComponentsBuilder
+                    .fromCurrentRequest()
+                    .replacePath("api/status/")
+                    .path(uploadTask.getId())
+                    .build()
+                    .toUri();
+
+            response.setHeader("Location", location.toString());
+            return ResponseEntity.accepted().build();
+
+        } catch (FileNotFoundException ex) {
+            throw new ApiException(HttpStatus.UNPROCESSABLE_ENTITY, "No file found at given URL", ex);
+        } catch (UnknownHostException ex) {
+            throw new ApiException(HttpStatus.UNPROCESSABLE_ENTITY, url.getHost() + ": host not found. ", ex);
         } catch (IOException ex) {
-            throw new HttpResponseException(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage(), ex);
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, null, ex);
+            throw new ApiException(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage(), ex);
         }
     }
 }
