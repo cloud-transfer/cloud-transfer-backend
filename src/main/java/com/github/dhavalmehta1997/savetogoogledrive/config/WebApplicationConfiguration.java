a5ca51ed148726667e37c38759a953a25bf21e3d
Dhaval Mehta <dhaval.mehta197@gmail.com>
2017-10-17 00:00:22 +0530

    Add swagger to provide rest api documentation

--- src/main/java/com/github/dhavalmehta1997/savetogoogledrive/config/WebApplicationConfiguration.java
+++ src/main/java/com/github/dhavalmehta1997/savetogoogledrive/config/WebApplicationConfiguration.java
@@ -5,35 +5,30 @@
  */
 package com.github.dhavalmehta1997.savetogoogledrive.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.JstlView;
 
 /**
  * @author Dhaval
  */
 @Configuration
 @EnableWebMvc
+@Import(SwaggerConfig.class)
 @ComponentScan(basePackages = "com.github.dhavalmehta1997.savetogoogledrive")
 public class WebApplicationConfiguration extends WebMvcConfigurerAdapter {
-
-    @Bean
-    public InternalResourceViewResolver viewResolver() {
-        InternalResourceViewResolver viewResolver
-                = new InternalResourceViewResolver();
-        viewResolver.setViewClass(JstlView.class);
-        viewResolver.setPrefix("/WEB-INF/views/jsp/");
-        viewResolver.setSuffix(".jsp");
-        return viewResolver;
-    }
-
     @Override
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
         configurer.enable();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/swagger-ui.html");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
