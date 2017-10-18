c9868e34c038adabf1d224b9f932e78237437a0c
Dhaval Mehta <dhaval.mehta197@gmail.com>
2017-10-19 02:28:39 +0530

    add serial version UID

--- src/main/java/com/github/dhavalmehta1997/savetogoogledrive/exception/ApiException.java
+++ src/main/java/com/github/dhavalmehta1997/savetogoogledrive/exception/ApiException.java
@@ -3,42 +3,44 @@ package com.github.dhavalmehta1997.savetogoogledrive.exception;
 import org.springframework.http.HttpStatus;
 
 public class ApiException extends RuntimeException {
-    private HttpStatus status;
 
-    public ApiException(HttpStatus status) {
-        this(status, null, null);
-    }
+	private static final long serialVersionUID = 1L;
 
-    public ApiException(HttpStatus status, String message) {
-        this(status, message, null);
-    }
+	private HttpStatus status;
 
-    public ApiException(HttpStatus status, String message, Throwable cause) {
-        super(message, cause);
-        this.status = status;
-    }
+	public ApiException(HttpStatus status) {
+		this(status, null, null);
+	}
 
-    public ApiException(HttpStatus status, Throwable cause) {
-        this(status, null, cause);
-    }
+	public ApiException(HttpStatus status, String message) {
+		this(status, message, null);
+	}
 
-    public ApiException(HttpStatus status, String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
-        this.status = status;
-    }
+	public ApiException(HttpStatus status, String message, Throwable cause) {
+		super(message, cause);
+		this.status = status;
+	}
 
-    public HttpStatus getStatus() {
-        return status;
-    }
+	public ApiException(HttpStatus status, Throwable cause) {
+		this(status, null, cause);
+	}
 
-    public void setStatus(HttpStatus status) {
-        this.status = status;
-    }
+	public ApiException(HttpStatus status, String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		this.status = status;
+	}
 
-    @Override
-    public String toString() {
-        return "ApiException{" +
-                "status=" + status +
-                '}';
-    }
+	public HttpStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(HttpStatus status) {
+		this.status = status;
+	}
+
+	@Override
+	public String toString() {
+		return "ApiException{" + "status=" + status + '}';
+	}
 }
