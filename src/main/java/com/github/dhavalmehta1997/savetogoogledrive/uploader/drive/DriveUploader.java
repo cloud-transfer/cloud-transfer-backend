ac6bfba66a0ab315d1f06c170d77ff7f908ba0a4
Dhaval Mehta <dhaval.mehta197@gmail.com>
2017-10-19 03:19:18 +0530

    Bug Fix & performance improvement

--- src/main/java/com/github/dhavalmehta1997/savetogoogledrive/uploader/drive/DriveUploader.java
+++ src/main/java/com/github/dhavalmehta1997/savetogoogledrive/uploader/drive/DriveUploader.java
@@ -1,129 +1,133 @@
 package com.github.dhavalmehta1997.savetogoogledrive.uploader.drive;
 
+import static com.github.dhavalmehta1997.savetogoogledrive.utility.HttpUtilities.USER_AGENT;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.validation.constraints.NotNull;
+
+import org.apache.commons.io.IOUtils;
+import org.springframework.http.HttpStatus;
+
 import com.github.dhavalmehta1997.savetogoogledrive.exception.ApiException;
 import com.github.dhavalmehta1997.savetogoogledrive.model.DownloadFileInfo;
 import com.github.dhavalmehta1997.savetogoogledrive.model.UploadInformation;
 import com.github.dhavalmehta1997.savetogoogledrive.model.UploadStatus;
 import com.github.dhavalmehta1997.savetogoogledrive.model.User;
 import com.github.dhavalmehta1997.savetogoogledrive.uploader.Uploader;
-import com.github.dhavalmehta1997.savetogoogledrive.utility.HttpUtilities;
 import com.google.gson.JsonObject;
-import org.springframework.http.HttpStatus;
-
-import javax.validation.constraints.NotNull;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.PrintStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-import static com.github.dhavalmehta1997.savetogoogledrive.utility.HttpUtilities.USER_AGENT;
-import static com.github.dhavalmehta1997.savetogoogledrive.utility.IOUtilities.inputStreamToString;
 
 abstract class DriveUploader implements Uploader {
-    private static final URL POST_URL;
-    private static final Logger LOGGER;
+	private static final URL CREATE_FILE_URL;
 
-    static {
-        LOGGER = Logger.getLogger(DriveUploader.class.getName());
-        try {
-            POST_URL = new URL("https://www.googleapis.com/upload/drive/v3/files?uploadType=resumable");
-        } catch (MalformedURLException ex) {
-            throw new ExceptionInInitializerError(ex);
-        }
-    }
+	static {
+		try {
+			CREATE_FILE_URL = new URL("https://www.googleapis.com/upload/drive/v3/files?uploadType=resumable");
+		} catch (MalformedURLException ex) {
+			throw new ExceptionInInitializerError(ex);
+		}
+	}
 
-    final int chunkSize;
-    protected User user;
-    DownloadFileInfo downloadFileInfo;
-    UploadInformation uploadInformation;
-    private URL PUT_URL;
+	final int chunkSize;
+	protected User user;
+	DownloadFileInfo downloadFileInfo;
+	UploadInformation uploadInformation;
+	private URL createdFileUrl;
 
-    DriveUploader(DownloadFileInfo downloadFileInfo, User user) {
-        this();
-        this.downloadFileInfo = downloadFileInfo;
-        this.user = user;
-        uploadInformation.setFileName(downloadFileInfo.getFileName());
-        uploadInformation.setUrl(downloadFileInfo.getUploadUrl().toString());
-        uploadInformation.setTotalSize(downloadFileInfo.getContentLength());
-        try {
-            obtainUploadUrl();
-        } catch (Exception e) {
-            uploadInformation.setUploadStatus(UploadStatus.failed);
-            uploadInformation.setErrorMessage(e.getMessage());
-            uploadInformation.setError(e);
-            LOGGER.log(Level.SEVERE, "Exception while initializing" + DriveUploader.class.getName(), e);
-        }
-    }
+	DriveUploader(DownloadFileInfo downloadFileInfo, User user) {
+		this();
+		this.user = user;
+		this.downloadFileInfo = downloadFileInfo;
+		uploadInformation.setFileName(downloadFileInfo.getFileName());
+		uploadInformation.setUrl(downloadFileInfo.getUploadUrl().toString());
+		uploadInformation.setTotalSize(downloadFileInfo.getContentLength());
 
-    private DriveUploader() {
-        chunkSize = 1024 * 1024; // 1 MB
-        uploadInformation = new UploadInformation();
-        uploadInformation.setUploadStatus(UploadStatus.waiting);
-    }
+		try {
+			obtainUploadUrl();
+		} catch (Exception e) {
+			uploadInformation.setUploadStatus(UploadStatus.failed);
+			uploadInformation.setErrorMessage(e.getMessage());
+		}
+	}
 
-    /**
-     * It will upload bytes in range [start,end] to Google drive.
-     *
-     * @param start starting byte of range
-     * @param end   ending byte of range
-     * @throws IOException
-     */
-    void uploadPartially(@NotNull byte[] buffer, long start, long end) throws IOException {
-        HttpURLConnection uploadConnection = (HttpURLConnection) PUT_URL.openConnection();
-        uploadConnection.setDoOutput(true);
-        uploadConnection.setRequestProperty("User-Agent", USER_AGENT);
-        String contentRange = "bytes " + start + "-" + end + "/" + downloadFileInfo.getContentLength();
-        uploadConnection.setRequestProperty("Content-Range", contentRange);
-        OutputStream out = uploadConnection.getOutputStream();
-        out.write(buffer, 0, (int) (end - start + 1));
-        out.close();
-        int statusCode = uploadConnection.getResponseCode();
-        // In case of successful upload, status code will be 3** or 2**
-        if (statusCode < 400)
-            uploadInformation.setUploadedSize(end + 1);
-        else {
-            String description = inputStreamToString(uploadConnection.getInputStream());
-            uploadInformation.setError(new ApiException(HttpStatus.valueOf(statusCode), description, null));
-        }
-    }
+	private DriveUploader() {
+		chunkSize = 1024 * 1024; // 1 MB
+		uploadInformation = new UploadInformation();
+		uploadInformation.setUploadStatus(UploadStatus.waiting);
 
-    private void obtainUploadUrl() throws IOException {
-        user.refreshTokenIfNecessary();
-        HttpURLConnection connection = (HttpURLConnection) POST_URL.openConnection();
-        connection.setRequestMethod("POST");
-        connection.setRequestProperty("User-Agent", USER_AGENT);
-        JsonObject jsonObject = new JsonObject();
-        jsonObject.addProperty("name", downloadFileInfo.getFileName());
-        String postBody = jsonObject.toString();
+	}
 
-        connection.setDoOutput(true);
-        connection.setRequestProperty("Content-Type", "application/json");
-        connection.setRequestProperty("Authorization", user.getToken().getTokenType() + " " + user.getToken().getAccessToken());
-        connection.setRequestProperty("X-Upload-Content-Type", downloadFileInfo.getContentType());
-        connection.setRequestProperty("X-Upload-Content-Length", String.valueOf(downloadFileInfo.getContentLength()));
+	/**
+	 * It will upload bytes in range [start,end] to Google drive.
+	 *
+	 * @param start
+	 *            starting byte of range
+	 * @param end
+	 *            ending byte of range
+	 */
+	void uploadPartially(@NotNull byte[] buffer, long start, long end) {
+		String contentRange = "bytes " + start + "-" + end + "/" + downloadFileInfo.getContentLength();
 
-        try (PrintStream writer = new PrintStream(connection.getOutputStream())) {
-            writer.print(postBody);
-        }
+		try {
+			HttpURLConnection uploadConnection = (HttpURLConnection) createdFileUrl.openConnection();
+			uploadConnection.setDoOutput(true);
+			uploadConnection.setRequestProperty("User-Agent", USER_AGENT);
+			uploadConnection.setRequestProperty("Content-Range", contentRange);
+			IOUtils.copy(new ByteArrayInputStream(buffer), uploadConnection.getOutputStream());
+			uploadConnection.connect();
+			int statusCode = uploadConnection.getResponseCode();
 
-        connection.connect();
+			// In case of successful upload, status code will be 3** or 2**
+			if (statusCode < 400)
+				uploadInformation.setUploadedSize(end + 1);
+			else {
+				throw new RuntimeException("Error While uploading file. Status code: " + statusCode);
+			}
+		} catch (Exception e) {
+			throw new RuntimeException("Error While uploading file.", e);
+		}
+	}
 
-        int statusCode = connection.getResponseCode();
+	private void obtainUploadUrl() throws IOException {
+		user.refreshTokenIfNecessary();
+		HttpURLConnection connection = (HttpURLConnection) CREATE_FILE_URL.openConnection();
+		connection.setRequestMethod("POST");
+		connection.setRequestProperty("User-Agent", USER_AGENT);
+		JsonObject jsonObject = new JsonObject();
+		jsonObject.addProperty("name", downloadFileInfo.getFileName());
+		String postBody = jsonObject.toString();
 
-        if (HttpUtilities.success(statusCode))
-            PUT_URL = new URL(connection.getHeaderField("Location"));
-        else {
-            String description = inputStreamToString(connection.getInputStream());
-            uploadInformation.setError(new ApiException(HttpStatus.valueOf(statusCode), description, null));
-        }
-    }
+		connection.setDoOutput(true);
+		connection.setRequestProperty("Content-Type", "application/json");
+		connection.setRequestProperty("Authorization",
+				user.getToken().getTokenType() + " " + user.getToken().getAccessToken());
+		connection.setRequestProperty("X-Upload-Content-Type", downloadFileInfo.getContentType());
+		connection.setRequestProperty("X-Upload-Content-Length", String.valueOf(downloadFileInfo.getContentLength()));
 
-    @Override
-    public UploadInformation getUploadInformation() {
-        return uploadInformation;
-    }
+		try (PrintStream writer = new PrintStream(connection.getOutputStream())) {
+			writer.print(postBody);
+		}
+
+		connection.connect();
+
+		int statusCode = connection.getResponseCode();
+
+		if (statusCode == HttpStatus.OK.value())
+			createdFileUrl = new URL(connection.getHeaderField("Location"));
+		else if (statusCode == HttpStatus.UNAUTHORIZED.value())
+			throw new ApiException(HttpStatus.UNAUTHORIZED, "Your session is expired");
+		else
+			throw new ApiException(HttpStatus.INTERNAL_SERVER_ERROR, "Cannot create new file in google dirve.");
+
+	}
+
+	@Override
+	public UploadInformation getUploadInformation() {
+		return uploadInformation;
+	}
 }
--- src/main/java/com/github/dhavalmehta1997/savetogoogledrive/uploader/drive/ResumableDriveUploader.java
+++ src/main/java/com/github/dhavalmehta1997/savetogoogledrive/uploader/drive/ResumableDriveUploader.java
@@ -1,67 +1,77 @@
 package com.github.dhavalmehta1997.savetogoogledrive.uploader.drive;
 
-import com.github.dhavalmehta1997.savetogoogledrive.model.DownloadFileInfo;
-import com.github.dhavalmehta1997.savetogoogledrive.model.UploadStatus;
-import com.github.dhavalmehta1997.savetogoogledrive.model.User;
-import org.apache.commons.io.IOUtils;
+import static com.github.dhavalmehta1997.savetogoogledrive.utility.HttpUtilities.USER_AGENT;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import static com.github.dhavalmehta1997.savetogoogledrive.utility.HttpUtilities.USER_AGENT;
+import org.apache.commons.io.IOUtils;
+
+import com.github.dhavalmehta1997.savetogoogledrive.model.DownloadFileInfo;
+import com.github.dhavalmehta1997.savetogoogledrive.model.UploadStatus;
+import com.github.dhavalmehta1997.savetogoogledrive.model.User;
 
 public class ResumableDriveUploader extends DriveUploader {
 
-    ResumableDriveUploader(DownloadFileInfo downloadFileInfo, User user) {
-        super(downloadFileInfo, user);
-    }
+	private static final Logger LOGGER = Logger.getLogger(ResumableDriveUploader.class.getName());
 
-    private byte[] downloadPartially(long start, long end) throws IOException {
-        HttpURLConnection downloadConnection = (HttpURLConnection) downloadFileInfo.getUploadUrl().openConnection();
-        downloadConnection.setRequestMethod("GET");
-        String rangeHeaderValue = "bytes=" + start + "-" + end;
-        downloadConnection.setRequestProperty("Range", rangeHeaderValue);
-        downloadConnection.setRequestProperty("User-Agent", USER_AGENT);
-        InputStream in = downloadConnection.getInputStream();
-        return IOUtils.toByteArray(in);
-    }
+	ResumableDriveUploader(DownloadFileInfo downloadFileInfo, User user) {
+		super(downloadFileInfo, user);
+	}
 
+	private byte[] downloadPartially(long start, long end) {
+		try {
+			HttpURLConnection downloadConnection = (HttpURLConnection) downloadFileInfo.getUploadUrl().openConnection();
 
-    /**
-     * This function will upload file to Google drive using resumable upload
-     * method.
-     */
-    @Override
-    public void upload() {
-        try {
-            uploadInformation.setUploadStatus(UploadStatus.uploading);
+			downloadConnection.setRequestMethod("GET");
+			String rangeHeaderValue = "bytes=" + start + "-" + end;
+			downloadConnection.setRequestProperty("Range", rangeHeaderValue);
+			downloadConnection.setRequestProperty("User-Agent", USER_AGENT);
+			InputStream in = downloadConnection.getInputStream();
+			return IOUtils.toByteArray(in);
+		} catch (Exception e) {
+			throw new RuntimeException("Can not download file", e);
+		}
+	}
 
-            while (uploadInformation.getUploadedSize() < downloadFileInfo.getContentLength()) {
-                long start = uploadInformation.getUploadedSize();
-                long end = uploadInformation.getUploadedSize() + chunkSize - 1;
+	/**
+	 * This function will upload file to Google drive using resumable upload method.
+	 */
+	@Override
+	public void upload() {
+		try {
+			uploadInformation.setUploadStatus(UploadStatus.uploading);
 
-                if (end >= downloadFileInfo.getContentLength())
-                    end = downloadFileInfo.getContentLength() - 1;
+			while (uploadInformation.getUploadedSize() < downloadFileInfo.getContentLength()) {
+				long start = uploadInformation.getUploadedSize();
+				long end = uploadInformation.getUploadedSize() + chunkSize - 1;
 
-                long startTime = System.currentTimeMillis();
-                byte[] buffer = downloadPartially(start, end);
-                uploadPartially(buffer, start, end);
-                long endTime = System.currentTimeMillis();
+				if (end >= downloadFileInfo.getContentLength())
+					end = downloadFileInfo.getContentLength() - 1;
 
-                uploadInformation.setUploadedSize(end + 1);
-                double dataFetched = end - start + 1;
-                double timeElapsed = (endTime - startTime) / 1000.0;
-                uploadInformation.setSpeed(dataFetched / timeElapsed);
-            }
+				long startTime = System.currentTimeMillis();
+				byte[] buffer = downloadPartially(start, end);
+				uploadPartially(buffer, start, end);
+				long endTime = System.currentTimeMillis();
 
-            uploadInformation.setUploadStatus(UploadStatus.completed);
+				uploadInformation.setUploadedSize(end + 1);
+				double dataFetched = end - start + 1;
+				double timeElapsed = (endTime - startTime) / 1000.0;
+				uploadInformation.setSpeed(dataFetched / timeElapsed);
+			}
 
-        } catch (Throwable ex) {
-            uploadInformation.setError(ex);
-            Logger.getLogger(ResumableDriveUploader.class.getName()).log(Level.SEVERE, ex.getMessage(), ex);
-        }
-    }
+			uploadInformation.setUploadStatus(UploadStatus.completed);
+
+		} catch (Throwable e) {
+			handleError(e);
+		}
+	}
+
+	private void handleError(Throwable e) {
+		uploadInformation.setUploadStatus(UploadStatus.failed);
+		uploadInformation.setErrorMessage(e.getMessage());
+		LOGGER.log(Level.SEVERE, null, e);
+	}
 }
