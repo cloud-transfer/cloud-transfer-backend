dc77b8f8fc9c4d21456d4d4e9c228a205997a3bc
Dhaval Mehta <dhaval.mehta197@gmail.com>
2017-10-22 21:29:07 +0530

    create spring boot application

---
+++ src/main/java/com/github/dhavalmehta1997/savetogoogledrive/SaveToGoogleDriveApplication.java
@@ -0,0 +1,14 @@
+package com.github.dhavalmehta1997.savetogoogledrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SaveToGoogleDriveApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(SaveToGoogleDriveApplication.class, args);
+	}
+
+}
--- src/main/java/com/github/dhavalmehta1997/savetogoogledrive/config/SwaggerConfig.java
+++ src/main/java/com/github/dhavalmehta1997/savetogoogledrive/config/SwaggerConfig.java
@@ -1,6 +1,8 @@
 package com.github.dhavalmehta1997.savetogoogledrive.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -10,25 +12,18 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@Configuration
 @EnableSwagger2
 public class SwaggerConfig {
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .useDefaultResponseMessages(false)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build()
-                .apiInfo(apiInfo());
-    }
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).useDefaultResponseMessages(false).select()
+				.apis(RequestHandlerSelectors.any()).paths(PathSelectors.any()).build().apiInfo(apiInfo());
+	}
 
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("Save to Drive Rest API")
-                .description("It is a free open source api to upload file from url directly to Google Drive.")
-                .contact(new Contact("Dhaval Mehta", null, "dhaval.mehta197@gmail.com"))
-                .version("1.0")
-                .build();
-    }
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder().title("Save to Drive Rest API")
+				.description("It is a free open source api to upload file from url directly to Google Drive.")
+				.contact(new Contact("Dhaval Mehta", null, "dhaval.mehta197@gmail.com")).version("1.0").build();
+	}
 }
