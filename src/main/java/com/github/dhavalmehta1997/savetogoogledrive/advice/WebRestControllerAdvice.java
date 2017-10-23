1a47246353a741d0eb4d3d8d7e670eeed8f20fe7
Dhaval Mehta <dhaval.mehta197@gmail.com>
2017-10-23 20:06:00 +0530

    handle HttpRequestMethodNotSupportedException

--- src/main/java/com/github/dhavalmehta1997/savetogoogledrive/advice/WebRestControllerAdvice.java
+++ src/main/java/com/github/dhavalmehta1997/savetogoogledrive/advice/WebRestControllerAdvice.java
@@ -1,28 +1,36 @@
 package com.github.dhavalmehta1997.savetogoogledrive.advice;
 
-import com.github.dhavalmehta1997.savetogoogledrive.exception.ApiException;
-import com.github.dhavalmehta1997.savetogoogledrive.model.ApiError;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import com.github.dhavalmehta1997.savetogoogledrive.exception.ApiException;
+import com.github.dhavalmehta1997.savetogoogledrive.model.ApiError;
 
 @ControllerAdvice
 public class WebRestControllerAdvice {
 
-    @ExceptionHandler(ApiException.class)
-    public ResponseEntity<ApiError> handleApiException(ApiException ex) {
-        ApiError apiError = new ApiError(ex.getStatus(), ex.getMessage(), null);
-        return new ResponseEntity<>(apiError, apiError.getStatus());
-    }
+	@ExceptionHandler(ApiException.class)
+	public ResponseEntity<ApiError> handleApiException(ApiException ex) {
+		ApiError apiError = new ApiError(ex.getStatus(), ex.getMessage(), null);
+		return new ResponseEntity<>(apiError, apiError.getStatus());
+	}
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<ApiError> handleGenericException(Exception ex) {
-        Logger.getLogger(getClass().getName()).log(Level.SEVERE, null, ex);
-        ApiError apiError = new ApiError(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage(), null);
-        return new ResponseEntity<>(apiError, HttpStatus.INTERNAL_SERVER_ERROR);
-    }
+	@ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+	public ResponseEntity<ApiError> handleMethodNotSupportedException(HttpRequestMethodNotSupportedException ex) {
+		ApiError apiError = new ApiError(HttpStatus.METHOD_NOT_ALLOWED, ex.getMessage(), null);
+		return new ResponseEntity<>(apiError, apiError.getStatus());
+	}
+
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<ApiError> handleGenericException(Exception ex) {
+		Logger.getLogger(getClass().getName()).log(Level.SEVERE, null, ex);
+		ApiError apiError = new ApiError(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage(), null);
+		return new ResponseEntity<>(apiError, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
 }
