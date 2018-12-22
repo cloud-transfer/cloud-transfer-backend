dea91849b4e37d571c456a3a7891fa27041b67a4
dhaval-mehta <dhaval.mehta197@gmail.com>
2018-12-23 00:24:23 +0530

    create url to goolge drive transfer service

---
+++ src/main/java/com/github/dhaval_mehta/cloud_transfer/transfer_service/TransferService.java
@@ -0,0 +1,44 @@
+package com.github.dhaval_mehta.cloud_transfer.transfer_service;
+
+import com.github.dhaval_mehta.cloud_transfer.downloader.URLDownloader;
+import com.github.dhaval_mehta.cloud_transfer.model.DownloadInformation;
+import com.github.dhaval_mehta.cloud_transfer.model.TransferStatus;
+import com.github.dhaval_mehta.cloud_transfer.uploader.Uploader;
+
+import javax.validation.constraints.NotNull;
+import java.io.IOException;
+
+public abstract class TransferService implements Runnable {
+
+    protected DownloadInformation downloadInformation;
+
+
+    public void run() {
+        transfer();
+    }
+
+    protected abstract @NotNull Uploader getUploader();
+
+    protected abstract URLDownloader getDownloader() throws IOException;
+
+    protected void transfer(){
+        try {
+            changeStatus(TransferStatus.Downloading);
+            var downloader = getDownloader();
+            downloadInformation = downloader.download();
+            changeStatus(TransferStatus.Uploading);
+            var uploader = getUploader();
+            uploader.upload();
+            changeStatus(TransferStatus.Finished);
+        } catch (Exception e){
+            changeStatus(TransferStatus.Failed);
+        }
+    }
+
+    private void changeStatus(TransferStatus status){
+        System.out.println(status);
+    }
+
+
+
+}
---
+++ src/main/java/com/github/dhaval_mehta/cloud_transfer/transfer_service/UrlToGoogleDriveTransferService.java
@@ -0,0 +1,47 @@
+package com.github.dhaval_mehta.cloud_transfer.transfer_service;
+
+import com.github.dhaval_mehta.cloud_transfer.downloader.URLDownloader;
+import com.github.dhaval_mehta.cloud_transfer.model.token.GoogleDriveToken;
+import com.github.dhaval_mehta.cloud_transfer.uploader.GoogleDriveUploader;
+import com.github.dhaval_mehta.cloud_transfer.uploader.Uploader;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.util.MultiValueMap;
+
+import javax.validation.constraints.NotNull;
+import java.io.File;
+import java.net.URI;
+
+public class UrlToGoogleDriveTransferService extends TransferService {
+
+    @Value("${downloadDirectory}")
+    private static String downloadDirectory;
+    private final MultiValueMap<String, String> headers;
+    private GoogleDriveToken token;
+    private String fileName;
+    private URI downloadUri;
+
+    public UrlToGoogleDriveTransferService(URI downloadUri, GoogleDriveToken token, MultiValueMap<String, String> headers, String fileName) {
+        this(downloadUri, token, headers);
+        this.fileName = fileName;
+    }
+
+    public UrlToGoogleDriveTransferService(URI downloadUri, GoogleDriveToken token, MultiValueMap<String, String> headers) {
+        this.token = token;
+        this.downloadUri = downloadUri;
+        this.headers = headers;
+    }
+
+
+    @Override
+    protected @NotNull Uploader getUploader() {
+        return new GoogleDriveUploader(downloadInformation.getFile(), token, downloadInformation.getContentType());
+    }
+
+    @Override
+    protected URLDownloader getDownloader() {
+        if (fileName == null)
+            return new URLDownloader(downloadUri, headers, new File(downloadDirectory));
+        else
+            return new URLDownloader(downloadUri, headers, new File(downloadDirectory), fileName);
+    }
+}
