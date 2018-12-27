e2364cd7196f14137cb506b105bbdc97130da233
dhaval-mehta <dhaval.mehta197@gmail.com>
2018-12-27 18:18:34 +0530

    create getter and setters

--- src/main/java/com/github/cloud_transfer/cloud_transfer_backend/model/token/GoogleDriveToken.java
+++ src/main/java/com/github/cloud_transfer/cloud_transfer_backend/model/token/GoogleDriveToken.java
@@ -3,6 +3,8 @@ package com.github.cloud_transfer.cloud_transfer_backend.model.token;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import lombok.Getter;
+import lombok.Setter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,6 +18,8 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.LocalDateTime;
 
+@Setter
+@Getter
 @Entity
 @DiscriminatorValue(value = TokenType.GoogleDriveToken)
 public class GoogleDriveToken extends Token {
