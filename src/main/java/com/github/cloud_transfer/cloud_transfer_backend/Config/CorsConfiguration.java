985c3d8c7841445973763e83335c28b5ac4ee922
dhaval-mehta <dhaval.mehta197@gmail.com>
2019-01-05 17:59:53 +0530

    add cors config

---
+++ src/main/java/com/github/cloud_transfer/cloud_transfer_backend/Config/CorsConfiguration.java
@@ -0,0 +1,20 @@
+package com.github.cloud_transfer.cloud_transfer_backend.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class CorsConfiguration {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
+    }
+}
